baf15b844282fb1a04f5a948a5bd85fff8dbd2cf
Grayson Shogan <graysontoddshogan@gmail.com>
2023-06-26 14:00:20 -0700

    done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -9,7 +9,9 @@ router.get("/", async (req, res) => {
     const posts = await Posts.find();
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving posts" });
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" });
   }
 });
 
@@ -21,43 +23,61 @@ router.get("/:id", async (req, res) => {
     if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found" });
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving the post" });
+    res
+      .status(500)
+      .json({ message: "The post information could not be retrieved" });
   }
 });
 
 // POST /api/posts
 router.post("/", async (req, res) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    res.status(400).json({ message: "Title and content are required" });
-  }
-  try {
-    const newPost = await Posts.add(req.body);
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json({ message: "Error creating the post" });
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    try {
+      const newPost = await Posts.add(req.body);
+      res.status(201).json(newPost);
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+          message: "There was an error while saving the post to the database",
+        });
+    }
   }
 });
 
 // PUT /api/posts/:id
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const { title, content } = req.body;
-  if (!title || !content) {
-    res.status(400).json({ message: "Title and content are required" });
-  }
-  try {
-    const updatedPost = await Posts.update(id, req.body);
-    if (updatedPost) {
-      res.status(200).json(updatedPost);
-    } else {
-      res.status(404).json({ message: "Post not found" });
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    try {
+      const updatedPost = await Posts.update(id, req.body);
+      if (updatedPost) {
+        res.status(200).json(updatedPost);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "The post information could not be modified" });
     }
-  } catch (error) {
-    res.status(500).json({ message: "Error updating the post" });
   }
 });
 
@@ -69,10 +89,12 @@ router.delete("/:id", async (req, res) => {
     if (deletedPost) {
       res.status(200).json(deletedPost);
     } else {
-      res.status(404).json({ message: "Post not found" });
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Error deleting the post" });
+    res.status(500).json({ message: "The post could not be removed" });
   }
 });
 
@@ -81,9 +103,17 @@ router.get("/:id/comments", async (req, res) => {
   const { id } = req.params;
   try {
     const comments = await Posts.findPostComments(id);
-    res.status(200).json(comments);
+    if (comments) {
+      res.status(200).json(comments);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    }
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving comments" });
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
   }
 });
 
